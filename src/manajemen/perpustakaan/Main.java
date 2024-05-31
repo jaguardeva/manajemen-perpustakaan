7e2b9aff87222cae27067edc795080d63375b03c
Jaguar Deva <jaguardeva54@gmail.com>
2024-05-31 17:24:19 +0700

    components: add side bar

--- src/manajemen/perpustakaan/Main.java
+++ src/manajemen/perpustakaan/Main.java
@@ -1,20 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package manajemen.perpustakaan;
 
-/**
- *
- * @author jagua
- */
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Membuat frame
+        NewJFrame frame = new NewJFrame();
+        frame.setTitle("Belajar GUI");
+
+       
+
+        // Menampilkan frame
+        frame.setVisible(true);
     }
-    
 }
